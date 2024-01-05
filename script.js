4cfa1f8003af463cf54b398482de6dc7cf230772
Emily Logothetis <Emily@Debras-Air.local>
2024-01-04 19:16:19 -0800

    ran_word for this commit: BALL

---
+++ script.js
@@ -0,0 +1,17 @@
+let num1 = document.getElementById("1")
+let num2 = document.getElementById("2")
+let num3 = document.getElementById("3")
+let num4 = document.getElementById("4")
+let num5 = document.getElementById("5")
+let num6 = document.getElementById("6")
+let num7 = document.getElementById("7")
+let num8 = document.getElementById("8")
+let num9 = document.getElementById("9")
+let plus = document.getElementById("plus")
+let minus = document.getElementById("minus")
+let times = document.getElementById("times")
+let divide = document.getElementById("divide")
+
+function calculator(){
+
+}
